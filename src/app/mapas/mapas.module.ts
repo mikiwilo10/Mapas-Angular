77025f8b590dc3f08de8e63216598e0d84920394
mikiwilo10 <mikiwilo10@gmail.com>
2023-07-09 16:32:17 -0500

    Inicio Mapas

---
+++ src/app/mapas/mapas.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapPageComponent } from './page/map-page/map-page.component';
+
+@NgModule({
+  declarations: [MapPageComponent],
+  imports: [CommonModule],
+  exports: [MapPageComponent],
+})
+export class MapasModule {}
